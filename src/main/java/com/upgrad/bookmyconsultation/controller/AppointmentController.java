f6078243f357ce7846fddda1d1b5230998c244c3
Divyank <dubeydivyank@gmail.com>
2022-05-05 16:17:45 +0530

    appointmentcontroller

--- src/main/java/com/upgrad/bookmyconsultation/controller/AppointmentController.java
+++ src/main/java/com/upgrad/bookmyconsultation/controller/AppointmentController.java
@@ -5,6 +5,7 @@ import com.upgrad.bookmyconsultation.exception.InvalidInputException;
 import com.upgrad.bookmyconsultation.exception.SlotUnavailableException;
 import com.upgrad.bookmyconsultation.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.security.krb5.internal.APOptions;
 
 @RestController
 @RequestMapping("/appointments")
@@ -21,6 +23,11 @@ public class AppointmentController {
 	private AppointmentService appointmentService;
 
 
+	@PostMapping()
+	public ResponseEntity<String> bookAppointment(@RequestBody Appointment bookAppointment){
+		String appointmentId = appointmentService.appointment(bookAppointment);
+		return new ResponseEntity<String>(appointmentId, HttpStatus.CREATED);
+	}
 	//create a method post method named bookAppointment with return type ReponseEntity
 		//method has paramter of type Appointment, use RequestBody Annotation for mapping
 	
@@ -29,7 +36,11 @@ public class AppointmentController {
 	
 	
 	
-	
+	@GetMapping("/{appointmentsId}")
+	public ResponseEntity<Appointment> getAppointment(@PathVariable String appointmentsId){
+		Appointment appointmentDetails = appointmentService.getAppointments(appointmentsId);
+		return new ResponseEntity<Appointment>(appointmentDetails, HttpStatus.OK);
+	}
 	//create a get method named getAppointment with return type as ResponseEntity
 		//method has appointmentId of type String. Use PathVariable annotation to identity appointment using the parameter defined
 		
